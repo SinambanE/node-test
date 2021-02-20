6b70a717910409ab13b36da85487c152ccdbae39
Eugene Sinamban <sinamban_e@cyberowl.co.jp>
2021-02-21 02:03:23 +0900

    add mongodb

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,5 @@
 alias node-test-build="docker-compose build node"
 alias node-test="node-test-build && docker-compose up node"
 alias node-test-bash="docker-compose exec node bash"
+alias node-test-mongo-bash="docker-compose exec db bash"
+alias node-test-mongo-logs="docker logs -f mongodb --tail 100"
